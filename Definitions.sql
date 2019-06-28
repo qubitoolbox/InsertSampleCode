c9b9c2114cad4b8aeca2f8ded24f476819ca1e38
qubitAI <osmanmorales@ymail.com>
2019-06-28 18:48:11 -0400

    Create Definitions.sql

---
+++ Definitions.sql
@@ -0,0 +1,18 @@
+INSERT INTO customer 
+VALUES 
+  (
+    'Rose Parker', '123   Thomas St.', 
+    'Toledo', 'OH', '57556', '801-653-2239'
+  );
+INSERT INTO customer 
+VALUES 
+  (
+    'Jessica Jones', '234 Pines St. ', 
+    'Los Angeles', 'CA', '34898', '831-552-2943'
+  );
+INSERT INTO customer 
+VALUES 
+  (
+    'John Peters', '345 Star  St.', 'Raleigh', 
+    'NC', '79999', '801-393-1122'
+  );
