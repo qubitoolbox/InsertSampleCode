9ba179175ab33d0a4d8e06ec60cab2e6a3e98ac7
qubitAI <osmanmorales@ymail.com>
2019-06-28 18:45:00 -0400

    Update Insert.sql

--- Insert.sql
+++ Insert.sql
@@ -1,31 +1,31 @@
 /* Airline database SQL data definition.
 */
-
+--Osman D MOrales 5661546
+--SET search_path = airline;
 -- if passenger table already exists, remove the table
-
-  drop table IF EXISTS passenger;
+drop 
+  table IF EXISTS customer cascade;
 -- if flight table already exists, remove the table
-
-  drop table IF EXISTS flight;
-  
+drop 
+  table IF EXISTS flight cascade;
 -- if departure table already exists, remove the tablema
-
-  drop table IF EXISTS departure cascade;
-
+drop 
+  table IF EXISTS booked_on cascade;
+-- if departure table already exists, remove the tablema
+drop 
+  table IF EXISTS departure cascade;
 -- if aircraft table already exists, remove the table
-
-  drop table IF EXISTS aircraft;
-
+drop 
+  table IF EXISTS aircraft;
 -- if personnel table already exists, remove the table
-
-  drop table IF EXISTS personnel cascade;
-
+drop 
+  table IF EXISTS employee cascade;
+drop 
+  table IF EXISTS assigned_to cascade;
 -- if plane_type table already exists, remove the table
-
-  drop table IF EXISTS plane_type;
-
+drop 
+  table IF EXISTS plane_type;
 -- if plane_type table already exists, remove the table
-
-  drop table IF EXISTS pilot cascade;
-
-/* Create all six empty tables of airline database */
+drop 
+  table IF EXISTS pilot cascade;
+--drop table IF EXISTS pilot cascade;
