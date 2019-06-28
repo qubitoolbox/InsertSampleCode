32a79643690ccf359e91c0e41408febcd0a3491e
qubitAI <osmanmorales@ymail.com>
2019-06-28 18:48:44 -0400

    Update Definitions.sql

--- Definitions.sql
+++ Definitions.sql
@@ -16,3 +16,37 @@ VALUES
     'John Peters', '345 Star  St.', 'Raleigh', 
     'NC', '79999', '801-393-1122'
   );
+
+INSERT INTO customer 
+VALUES 
+  (
+    'Bryan Brown', '435 Palm  St.', 'Miami', 
+    'FL', '30533', '801-933-2424'
+  );
+INSERT INTO customer 
+VALUES 
+  (
+    'Mark Williams', '348 Andrew  St.', 
+    'Fort Lauderdale', 'FL', '33318', 
+    '801-343-2321'
+  );
+INSERT INTO customer 
+VALUES 
+  (
+    'Carol Phillips', '395 Pine St.', 
+    'Omaha', 'NE', '88899', '301-318-2327'
+  );
+INSERT INTO customer 
+VALUES 
+  (
+    'Madison Parker', '285 Diamond  St.', 
+    'San Diego', 'CA', '99977', '301-493-1203'
+  );
+INSERT INTO customer 
+VALUES 
+  (
+    'Justin Colano', '223 Easy St.', 
+    'Tampa', 'FL', '66798', '701-193-2320'
+  );
+  
+  
